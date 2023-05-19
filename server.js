ed16b3cc35c2e8a33ef5be44531d1c05b89927a1
Mukeshkumar <mukeshkumar87086@gmail.com>
2023-05-19 10:31:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 let corsOptions = {
-  origin: ['http://localhost:4000'],
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
