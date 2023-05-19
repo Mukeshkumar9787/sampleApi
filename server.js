cfcc6fcb438943a93a9f35b92261b7a69c1482b2
Mukeshkumar <mukeshkumar87086@gmail.com>
2023-05-19 10:22:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import cors from 'cors';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express()
 app.use(express.json())
